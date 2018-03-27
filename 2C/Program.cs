4b5a18b21e8eeef54ac644f642b20cdb9c1855a3
xkevinx45 <kneely1995@gmail.com>
2018-03-26 21:51:54 -0400

    First Commit

--- 2C/Program.cs
+++ 2C/Program.cs
@@ -7,9 +7,35 @@ using System.Threading.Tasks;
 namespace _2C
 {
     class Program
-    {
-        static void Main(string[] args)
+    {       //Syntactic sugar is syntax designed to make things easier to read or to express.
+
+
+        static void Main(string[] args) => Bleed();
+
+        static void Bleed(string Me = "I", string Blood = "Orange")
         {
+            Console.WriteLine($"{Me} bleed {Blood}");
+            Manual.Me(); Manual.Blood();
+        }
+        static void Bleed(string Me = "I", string Blood = "Orange", string Time = "Always")
+        {
+            Console.WriteLine($"{Me} {Time} bleed {Blood}");
+            Manual.Me(); Manual.Time(); Manual.Blood();
+        }
+    }
+    class Manual
+    {
+        public static string Me()
+        {
+            return ("I");
+        }
+        public static string Blood()
+        {
+            return ("Orange");
+        }
+        public static string Time()
+        {
+            return ("");
         }
     }
 }
